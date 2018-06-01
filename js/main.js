64db5036f3489e6acede6b30df7259d3f989ba67
Laurie Carano <lauriecarano@gmail.com>
2018-05-31 21:33:13 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["Queen", "Queen", "King", "King"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped Queen")
+
+var cardTwo = cards[1];
+//	cardsInPlay.push(cardTwo);
+//	console.log("User flipped Queen")
+
+var cardThree = cards[2];
+	cardsInPlay.push(cardThree);
+	console.log("User flipped King")
+
+var cardFour = cards[3];
+//	cardsInPlay.push(cardFour);
+//	console.log("User flipped King");
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+} else {
+	console.log("Pick another card");
+}
