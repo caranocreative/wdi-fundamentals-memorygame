b99cabf9e4cbb78639a396f72a8f00fd200c8f68
Laurie Carano <lauriecarano@gmail.com>
2018-06-01 19:15:03 -0600

    Created game logic

--- js/main.js
+++ js/main.js
@@ -2,29 +2,28 @@ var cards = ["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped Queen")
+var checkForMatch = function(){
 
-var cardTwo = cards[1];
-//	cardsInPlay.push(cardTwo);
-//	console.log("User flipped Queen")
-
-var cardThree = cards[2];
-	cardsInPlay.push(cardThree);
-	console.log("User flipped King")
-
-var cardFour = cards[3];
-//	cardsInPlay.push(cardFour);
-//	console.log("User flipped King");
-
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
-	}
-} else {
-	console.log("Pick another card");
 }
+
+var flipCard = function (cardId) {
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			alert("Sorry, try again.")
+		}
+	}
+
+	console.log("User flipped " + cards[cardId]);
+
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
