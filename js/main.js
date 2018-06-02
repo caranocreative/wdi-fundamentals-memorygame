5051eecfda905bfd765d7c0109d705f7d0a65d75
Laurie Carano <lauriecarano@gmail.com>
2018-06-02 12:53:24 -0600

    added more code unit 10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"	
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"	
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"	
+},
+];
 
 var cardsInPlay = [];
 
@@ -8,7 +29,7 @@ var checkForMatch = function(){
 
 var flipCard = function (cardId) {
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -18,7 +39,9 @@ var flipCard = function (cardId) {
 		}
 	}
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("cardImage " + cards[cardId].cardImage);
+	console.log("suit " + cards[cardId].suit);
 
 checkForMatch();
 }
